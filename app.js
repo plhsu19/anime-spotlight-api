6764387267601fc451d407a472094e1c79df49f9
Pei-Lun Hsu <peilun.hsu@getyourguide.com>
2023-07-20 21:06:02 +0200

    [BE-03] add initial Anime Model, add routes, services folders

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const morgan = require('morgan');
 
 const app = express();
 
+// middleware to parse JSON based request payload
 app.use(express.json());
-
-if(process.env.NODE_ENV === "development") {
-    app.use(morgan('development'));
+// middleware to log the received requests
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  next();
 }
 
 app.use((req, res, next) => {
-    console.log(req.body);
-    next();
-})
-
+  console.log(req.body);
+});
 
 module.exports = app;
---
+++ models/Anime.js
@@ -0,0 +1,11 @@
+class Anime {
+    constructor(data) {
+        this.id = data.id;
+        this.title;
+        this.enTitle;
+        this.description;
+        this.rating;
+        this.startDate;
+        this.endDate;
+    }
+}
